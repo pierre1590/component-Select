9565309df0bfdd35d48e49d250462ba0a7d7c6d4
Piero Sabino <piero.sa@icloud.com>
2021-12-07 20:21:52 +0100

    Create component Select

---
+++ src/components/Select/Select.js
@@ -0,0 +1,39 @@
+import { FaCaretDown } from "react-icons/fa";
+import { useState } from "react";
+const options = [
+    {label:'Piero Sabino', value:'Piero Sabino'},
+    {label:'Edmondo di Ronza', value:'Edmondo di Ronza'},
+    {label:'Giorgio Coppola', value:'Giorgio Coppola'},
+    {label:'Francesco Rua', value:'Francesco Rua'},
+];
+
+function Select({selected, setSelected}) {
+  const [isActive, setIsActive] = useState(false);
+  return (
+   
+    <div className="dropdown">
+     <h2 style={{textAlign:'center'}}>Select a member</h2>
+      <div className="dropdown-btn" onClick={(e) => setIsActive(!isActive)}>
+        {selected}
+        <FaCaretDown />
+      </div>
+      {isActive && (
+        <div className="dropdown-content">
+          {options.map((option) => (
+            <div
+              onClick={(e) => {
+                setSelected(option.value);
+                setIsActive(false);
+              }}
+              className="dropdown-item"
+            >
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Select;
